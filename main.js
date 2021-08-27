042258d87a5a013aa7e0793e43e5b141227e469c
Tania Rodriguez Fdez <taniacsif@gmail.com>
2021-08-27 09:13:55 +0200

    kkkkk

---
+++ main.js
@@ -0,0 +1,9 @@
+let addToShoppingCartButtons = document.querySelectorAll(".addToCart");
+addToShoppingCartButtons.forEach((addToCartButton)=>{addToCartButtons.addEventListener("click",addToCartClicked);});
+
+function addToCartCliked(event){
+    let button = event.target;
+    let conatinerImg = buttom.closest(".container-img");
+
+    let conatinerImgTitle = containerImg.querySelector(".containerImg-title");
+}
