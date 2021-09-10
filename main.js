0870c9324d45dde35521170dbc0d5dd937dff950
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2021-09-10 06:10:33 -0300

    Finaalll

--- main.js
+++ main.js
@@ -1,120 +1,12 @@
-function sumarCantidades (indice){
-    Menu[indice].cantidad++;
-    loadPlatos()
-    totalDeItems()
-    console.log(carrito)
-    loadPlatosPagDos()
-    
-}
+ let elementosMenu=document.getElementById("containerPlatos"); 
+ let elementosCarrito = document.getElementById('containerPlatos2');
+ let pagUno = document.getElementById('pagUno');
+ let pagDos = document.getElementById('pagDos');
+ let totalFactura = 0
+ let mensaje = ""
+ let carrito = [];
 
-let carrito = [];
-
-function upDateCarrito() {
-
-carrito = Menu.filter(element => element.cantidad > 0)
-}
-function restarCantidades (indice){
-    if(Menu[indice].cantidad > 0){
-        Menu[indice].cantidad--
-        loadPlatos()
-        totalDeItems()
-        loadPlatosPagDos()
-    } 
-}
-function cambiarPagina(){
-    ocultarPagUno();
-    mostrarPagDos();
-    loadPlatosPagDos();
-
-    calcularPrecios()
-}
-
-let pagUno = document.getElementById('pagUno');
-function ocultarPagUno(){
-    pagUno.classList.add('ocultar');
-}
-let pagDos = document.getElementById('pagDos');
-function mostrarPagDos(){
-
-    pagDos.classList.remove('movil2_bloqueado');
-    pagDos.classList.add('desbloqueado');
-    console.log('desbloqueadooo')
-}
-function volverAPagUno() {
-    pagDos.classList.remove('desbloqueado');
-    pagUno.classList.remove('ocultar')
-
-}
-function totalDeItems(){
-    let total = document.getElementById('totalItems');
-    
-        total.innerHTML = carrito.length
-}
-
-document.addEventListener('click', function(event) {
-        let sumar = event.target.dataset.sumar
-        let restar = event.target.dataset.restar
-        let cesta = event.target.dataset.cesta
-        let back = event.target.dataset.back
-
-        if(sumar){
-            sumarCantidades(sumar)
-            
-        }
-        if(restar){
-            restarCantidades(restar);
-        }
-        if(cesta){
-            cambiarPagina();
-        }
-        if(back){
-            volverAPagUno();
-        }
-
-    });
-
-let elementosMenu=document.getElementById("containerPlatos"); 
-let printPagDos = document.getElementById('containerPlatos2');
-function printPlatoPagDos(elementos, indice){
-
-    printPagDos.innerHTML +=`<div class="containerFood">
-
-                        <div class="col-md-6 box-foto">
-                            <img class="img-fluid" src= ${elementos.imagen} alt= ${elementos.alt}>
-                        </div>
-
-                        <div class="info-contador">
-                            <div class="info_food">
-                            <p class="precio_food"><span>$</span>
-                             ${elementos.price}
-                             </p>
-                            <h6 class="name_food">
-                             ${elementos.nombre}
-                             </h6>
-                            <p class="description_food">
-                            ${elementos.description}</p>
-                        </div>
-
-                        <div class="contador_food" id="${elementos.id}">
-                        
-
-                                <button data-restar = "${indice}" class="restar">-</button> 
-                                <input  type="number" class="num_contador" size="5" max-length="2" value="${elementos.cantidad}">   
-                                <button data-sumar ="${indice}" class="sumar">+</button> 
-
-                        </div>
-                    </div>
-                </div>`;
-}
-function loadPlatosPagDos(){
-    printPagDos.innerHTML = ""
-    carrito.forEach((elemento, index)=>printPlatoPagDos(elemento, index));
-    calcularPrecios()
-    }
-    
-
-
-let Menu = 
+ let Menu = 
 [
     {
         id: "Chiken Salad",
@@ -129,10 +21,10 @@ let Menu =
     {
         id: "Mutton Curry",
         nombre: 'Mutton Curry',
-        description: '',
+        description: 'With cheese sauce',
         price: 52,
         imagen: 'fotoscomida/Mutton-Curry.jpg',
-        alt:"comida Chiken",
+        alt:"comida Mutton Curry",
         cantidad: 0
 
     },
@@ -142,14 +34,15 @@ let Menu =
         description: 'With cheese sauce',
         price: 60,
         imagen: 'fotoscomida/french-fryes.jfif',
-        alt:"comida Chiken",
+        alt:"comida French Fryes",
         cantidad: 0
 
 
     }
  ];
 
-function printPlato(elementos, indice, listDishes){
+ //Funciones Imprimir Plato y Cargar pag1 y pag2
+ function printPlato(elementos, indice, listDishes){
 
     listDishes.innerHTML +=`<div class="containerFood">
 
@@ -181,18 +74,49 @@ function printPlato(elementos, indice, listDishes){
                 </div>`;
 }
 
-
 function loadPlatos(){
 elementosMenu.innerHTML = ""
 Menu.forEach((elemento, index)=>printPlato(elemento, index, elementosMenu ));
 upDateCarrito();
 calcularPrecios();
-}//index=posicón de cada uno de los elementos en el array menú 
+}
+function loadPlatosPagDos(){
+    elementosCarrito.innerHTML = ""
+    carrito.forEach((elemento, index)=>printPlato(elemento, index, elementosCarrito));
+    calcularPrecios()
+}
+ loadPlatos();
 
+//Funciones Aumentar & Restar Contador 
+function sumarCantidades (indice){
+    Menu[indice].cantidad++;
+    loadPlatos()
+    totalDeItems()
+    console.log(carrito)
+    loadPlatosPagDos()
+    
+}
+function restarCantidades (indice){
+    if(Menu[indice].cantidad > 0){
+        Menu[indice].cantidad--
+        loadPlatos()
+        totalDeItems()
+        loadPlatosPagDos()
+    } 
+}
 
+// Funciones Carrito
+function upDateCarrito() {
 
-let totalFactura = 0
+carrito = Menu.filter(element => element.cantidad > 0)
+}
+function totalDeItems(){
+    let total = document.getElementById('totalItems');
+    
+        total.innerHTML = carrito.length
+}
 
+// Funciones Calcular Precio e Imprimir
 function calcularPrecios (){
     let subTotal = 0
   
@@ -206,31 +130,92 @@ function calcularPrecios (){
     imprimirPrecios()
 
 }
-
 function imprimirPrecios (){
   let precioElement =  document.getElementById("price-total");
     precioElement.innerHTML = totalFactura
 }
 
-let mensaje = ""
+// Funciones de Página
 
+function cambiarPagina(){
+    ocultarPagUno();
+    mostrarPagDos();
+    loadPlatosPagDos();
 
+    calcularPrecios()
+}
+function ocultarPagUno(){
+    pagUno.classList.add('ocultar');
+}
+function mostrarPagDos(){
 
+    pagDos.classList.remove('movil2_bloqueado');
+    pagDos.classList.add('desbloqueado');
+}
+function volverAPagUno() {
+    pagDos.classList.remove('desbloqueado');
+    pagUno.classList.remove('ocultar');
+}
+
+// Funciones Crear y Enviar Mensaje
 function crearMensaje(){
     mensaje += "el usuario pidio:¨"
     carrito.forEach((items) =>{
         let precioPorPlato = items.price * items.cantidad
         mensaje += `${items.nombre} X ${items.cantidad} precio: ${precioPorPlato}`
     })
-
     mensaje += `"Total es ${totalFactura} "`
-    console.log(mensaje)
+    linkExterno()
+}
+function linkExterno(){
+    window.location = `https://api.whatsapp.com/send/?phone=643427167&text=${mensaje}`
 }
 
+// Listeners
+document.addEventListener('click', function(event) {
+        let sumar = event.target.dataset.sumar
+        let restar = event.target.dataset.restar
+        let cesta = event.target.dataset.cesta
+        let back = event.target.dataset.back
+
+        if(sumar){
+            sumarCantidades(sumar)
+            
+        }
+        if(restar){
+            restarCantidades(restar);
+        }
+        if(cesta){
+            cambiarPagina();
+        }
+        if(back){
+            volverAPagUno();
+        }
+
+    });
 
 
 
-loadPlatos();
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
